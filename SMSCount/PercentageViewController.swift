62375ae490d5e507e9105900b18c025122b59433
EddieWen-Taiwan <m20021991y@gmail.com>
2015-08-26 22:23:51 +0800

    Complete gesture in PercentageViewController. But I don't know how to integrate them to same code

--- SMSCount/PercentageViewController.swift
+++ SMSCount/PercentageViewController.swift
@@ -18,9 +18,24 @@ class PercentageViewController: UIViewController {
     // Create a new CircleView
     var circleView: PercentageCircleView!
 
+    var leftSwipeGesture: UISwipeGestureRecognizer!
+    var rightSwipeGesture: UISwipeGestureRecognizer!
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        leftSwipeGesture = UISwipeGestureRecognizer(target: self, action: Selector("switchBetweenView:"))
+        leftSwipeGesture.direction = .Left
+        rightSwipeGesture = UISwipeGestureRecognizer(target: self, action: Selector("switchBetweenView:"))
+        rightSwipeGesture.direction = .Right
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        self.view.addGestureRecognizer(leftSwipeGesture)
+        self.view.addGestureRecognizer(rightSwipeGesture)
 
         var chartHeight = self.view.frame.height
         var chartWidth = chartHeight
@@ -51,6 +66,12 @@ class PercentageViewController: UIViewController {
     func drawPercentageChart() {
         // Animate the drawing of the circle over the course of 1 second
         circleView.animateCircle( countingClass.getCurrentProgress()*(0.01) )
+    }
+
+    func switchBetweenView(sender: UISwipeGestureRecognizer) {
+
+        tabBarController?.selectedIndex = ( sender.direction == .Left ) ? 2 : 0
+
     }
 
     override func didReceiveMemoryWarning() {
