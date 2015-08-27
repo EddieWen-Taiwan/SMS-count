70183420994b52f4629029c48c4b4a60cac4dcf9
EddieWen-Taiwan <m20021991y@gmail.com>
2015-08-27 16:49:36 +0800

    almost completed BasicGestureViewController

--- SMSCount/BasicGestureViewController.swift
+++ SMSCount/BasicGestureViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class BasicGestureViewController: UIViewController {
 
+    var leftSwipeGesture: UISwipeGestureRecognizer!
+    var rightSwipeGesture: UISwipeGestureRecognizer!
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        leftSwipeGesture = UISwipeGestureRecognizer(target: self, action: Selector("switchBetweenView:"))
+        leftSwipeGesture.direction = .Left
+        rightSwipeGesture = UISwipeGestureRecognizer(target: self, action: Selector("switchBetweenView:"))
+        rightSwipeGesture.direction = .Right
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        self.view.addGestureRecognizer(leftSwipeGesture)
+        self.view.addGestureRecognizer(rightSwipeGesture)
     }
-    
+
+    func switchBetweenView(sender: UISwipeGestureRecognizer) {
+
+        println(sender)
+        println(self.view)
+
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- SMSCount/PercentageViewController.swift
+++ SMSCount/PercentageViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PercentageViewController: UIViewController {
+class PercentageViewController: BasicGestureViewController {
 
     @IBOutlet var percentageLabel: UILabel!
     @IBOutlet var percentSymbol: UILabel!
@@ -18,24 +18,24 @@ class PercentageViewController: UIViewController {
     // Create a new CircleView
     var circleView: PercentageCircleView!
 
-    var leftSwipeGesture: UISwipeGestureRecognizer!
-    var rightSwipeGesture: UISwipeGestureRecognizer!
+//    var leftSwipeGesture: UISwipeGestureRecognizer!
+//    var rightSwipeGesture: UISwipeGestureRecognizer!
 
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
 
-        leftSwipeGesture = UISwipeGestureRecognizer(target: self, action: Selector("switchBetweenView:"))
-        leftSwipeGesture.direction = .Left
-        rightSwipeGesture = UISwipeGestureRecognizer(target: self, action: Selector("switchBetweenView:"))
-        rightSwipeGesture.direction = .Right
+//        leftSwipeGesture = UISwipeGestureRecognizer(target: self, action: Selector("switchBetweenView:"))
+//        leftSwipeGesture.direction = .Left
+//        rightSwipeGesture = UISwipeGestureRecognizer(target: self, action: Selector("switchBetweenView:"))
+//        rightSwipeGesture.direction = .Right
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        self.view.addGestureRecognizer(leftSwipeGesture)
-        self.view.addGestureRecognizer(rightSwipeGesture)
+//        self.view.addGestureRecognizer(leftSwipeGesture)
+//        self.view.addGestureRecognizer(rightSwipeGesture)
 
         var chartHeight = self.view.frame.height
         var chartWidth = chartHeight
@@ -68,11 +68,11 @@ class PercentageViewController: UIViewController {
         circleView.animateCircle( countingClass.getCurrentProgress()*(0.01) )
     }
 
-    func switchBetweenView(sender: UISwipeGestureRecognizer) {
-
-        tabBarController?.selectedIndex = ( sender.direction == .Left ) ? 2 : 0
-
-    }
+//    func switchBetweenView(sender: UISwipeGestureRecognizer) {
+//
+//        tabBarController?.selectedIndex = ( sender.direction == .Left ) ? 2 : 0
+//
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
