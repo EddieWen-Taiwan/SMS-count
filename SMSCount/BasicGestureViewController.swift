3a2e17c173e0e5f58e611fc5d6f34d410b0dd7cc
EddieWen-Taiwan <m20021991y@gmail.com>
2015-08-27 16:43:00 +0800

    add BasicGestureViewController

---
+++ SMSCount/BasicGestureViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BasicGestureViewController.swift
+//  SMSCount
+//
+//  Created by Eddie on 2015/8/27.
+//  Copyright (c) 2015年 Wen. All rights reserved.
+//
+
+import UIKit
+
+class BasicGestureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
