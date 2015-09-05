b44e7c18fa9d5e5c91dbe2588f6a6d6eae030448
EddieWen-Taiwan <m20021991y@gmail.com>
2015-09-05 22:52:26 +0800

    Status setting without code

--- SMSCount/AppDelegate.swift
+++ SMSCount/AppDelegate.swift
@@ -18,7 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
 
-        UIApplication.sharedApplication().setStatusBarStyle( UIStatusBarStyle.LightContent, animated: false )
         UINavigationBar.appearance().barTintColor = UIColor(red: 255/255, green: 206/255, blue: 68/255, alpha: 100/100)
 
         return true
