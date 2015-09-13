0ce7e5f128ca929240b5763c9b15142de1f35f40
EddieWen-Taiwan <m20021991y@gmail.com>
2015-09-13 20:47:23 +0800

    add uiswitch to settingViewController

--- SMSCount/SettingViewController.swift
+++ SMSCount/SettingViewController.swift
@@ -30,6 +30,8 @@ class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerV
     @IBOutlet var discountDaysPickerElement: UIPickerView!
     var discountDaysPickerDataSource = [ "0 天", "1 天", "2 天", "3 天", "4 天", "5 天", "6 天", "7 天", "8 天", "9 天", "10 天",  "11 天", "12 天", "13 天", "14 天", "15 天", "16 天", "17 天", "18 天", "19 天", "20 天", "21 天", "22 天", "23 天", "24 天", "25 天", "26 天", "27 天", "28 天", "29 天", "30 天" ]
 
+    @IBOutlet var autoWeekendSwitch: UIView!
+
     let dateFormatter = NSDateFormatter()
     let userPreference = NSUserDefaults( suiteName: "group.EddieWen.SMSCount" )!
 
