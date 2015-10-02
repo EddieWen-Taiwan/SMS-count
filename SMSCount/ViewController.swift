22a47ed4e68b28f4fcfdb2f2408d631f4c0ac5df
EddieWen-Taiwan <m20021991y@gmail.com>
2015-10-02 23:03:42 +0800

    Remove else

--- SMSCount/ViewController.swift
+++ SMSCount/ViewController.swift
@@ -239,8 +239,6 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
                     self.checkDaysAnimation()
             })
 
-        } else {
-            print("animating")
         }
 
     }
