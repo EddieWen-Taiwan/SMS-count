d75848ff259e1c334ee64f2b2cc65f2402008b06
EddieWen-Taiwan <m20021991y@gmail.com>
2015-10-10 00:40:53 +0800

    Remove finish download remind

--- SMSCount/MonthlyImages.swift
+++ SMSCount/MonthlyImages.swift
@@ -70,8 +70,6 @@ class MonthlyImages {
     private func downloadImage( url: NSURL, backgroundImage: UIImageView ) {
         self.getImageFromUrl(url) { (data, response, error)  in
             dispatch_async( dispatch_get_main_queue() ) { () -> Void in
-                print("Finished downloading \"\(url.URLByDeletingPathExtension!.lastPathComponent!)\".")
-
                 self.saveImage( UIImage(data: data!)! )
                 self.userPreference.setObject( self.currentMonth, forKey: "backgroundMonth" )
                 backgroundImage.image = UIImage(data: data!)
