792d59a1969d826526089081c90cf5f4f0b219db
EddieWen-Taiwan <m20021991y@gmail.com>
2015-10-20 17:33:21 +0800

    FBSDKLoginButton is success. And get infomation of loggin and logout

--- SMSCount/SettingViewController.swift
+++ SMSCount/SettingViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 //import FBSDKCoreKit
 import FBSDKLoginKit
 
-class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, FBSDKLoginButtonDelegate {
 
     @IBOutlet var screenMask: UIView!
     @IBOutlet var FBLoginView: UIView!
@@ -75,18 +75,35 @@ class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerV
         }
         
         // FB Login
-        if FBSDKAccessToken.currentAccessToken() != nil {
+//        if FBSDKAccessToken.currentAccessToken() != nil {
             // User is login.
-        } else {
+//        } else {
             self.view.layoutIfNeeded()
 
             let loginView = FBSDKLoginButton()
             self.FBLoginView.addSubview( loginView )
             loginView.frame = CGRectMake( 0, 0, self.FBLoginView.frame.width, self.FBLoginView.frame.height )
             loginView.readPermissions = [ "public_profile", "email", "user_friends" ]
-            //            loginView.delegate = self
-        }
+            loginView.delegate = self
+//        }
 
+    }
+
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        print("User Logged In")
+        if ((error) != nil) {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // Navigate to other view
+        }
+    }
+
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User Logged Out")
     }
 
     override func viewDidAppear(animated: Bool) {
