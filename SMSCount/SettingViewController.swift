e9d5ece86a7731498e1850980542d348f05c0d6f
EddieWen-Taiwan <m20021991y@gmail.com>
2015-10-24 21:01:25 +0800

    Add User infomation In SettingViewController, after user login with FB account, app get the infomation of user and save them to Parse bt objectId.

--- SMSCount/SettingViewController.swift
+++ SMSCount/SettingViewController.swift
@@ -93,7 +93,6 @@ class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerV
                 discountDaysLabel.text = userDiscountDays
             }
         }
-
     }
 
     @IBAction func editEnterDate(sender: AnyObject) {
@@ -236,39 +235,23 @@ class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerV
                     if let FBID = result.objectForKey("id") {
                         print("User id : \(FBID)")
                         
-                        // Use FB_ID to query Parse data,
-                        // if userInfo exists, get "objectId" and update userPreference.
-                        // if userInfo doesn't exist, save infomations to Parse
-                        let selectUserQuery = PFQuery(className: "User")
-                        selectUserQuery.whereKey("fb_id", equalTo: FBID)
-                        selectUserQuery.findObjectsInBackgroundWithBlock({ (objects: [PFObject]?, error: NSError?) -> Void in
+                        // Update user email, name .... by objectId
+                        let userQuery = PFQuery(className: "User")
+                        userQuery.getObjectInBackgroundWithId( self.userPreference.stringForKey("UserID")! ) {
+                            (user: PFObject?, error: NSError?) -> Void in
                             if error == nil {
-                                if objects!.count > 0 {
-                                    // User exists
-                                    for user in objects! {
-                                        self.userPreference.setObject( user.objectId, forKey: "UserID" )
-                                    }
-                                } else {
-                                    // New user
-                                    let userInfo = PFObject(className: "User")
-                                    userInfo["fb_id"] = FBID
-                                    if let userName = result.objectForKey("name") {
-                                        print("User name : \(userName)")
-                                        userInfo["username"] = userName
-                                    }
-                                    if let userMail = result.objectForKey("email") {
-                                        print("User mail : \(userMail)")
-                                        userInfo["email"] = userMail
-                                    }
-                                    userInfo.saveInBackgroundWithBlock{ (success: Bool, error: NSError?) -> Void in
-                                        if success {
-                                            print("NEW objectId is \(userInfo.objectId)")
-                                            self.userPreference.setObject( userInfo.objectId, forKey: "UserID" )
-                                        }
-                                    }
-                                } // --- if objects.count
+                                print(user)
+
+                                user!.setObject( FBID, forKey: "fb_id" )
+                                if let userName = result.objectForKey("name") {
+                                    user!.setObject( userName, forKey: "username" )
+                                }
+                                if let userMail = result.objectForKey("email") {
+                                    user!.setObject( userMail, forKey: "userMail" )
+                                }
+                                user!.saveInBackground()
                             }
-                        }) // --- selectUserQuery
+                        }
                     }
                 } else {
                     print("ERROR : \(error)")
