0a6cc03ec6fadeb0f0b0b059f24708802ffd4a97
EddieWen-Taiwan <m20021991y@gmail.com>
2015-10-26 15:59:46 +0800

    Add a new class called 'UserInfo' to help app to save data to Parse

---
+++ SMSCount/UserInfo.swift
@@ -0,0 +1,22 @@
+//
+//  UserInfo.swift
+//  SMSCount
+//
+//  Created by Eddie on 10/26/15.
+//  Copyright © 2015 Wen. All rights reserved.
+//
+
+import Parse
+
+class UserInfo {
+
+    var objectStatus: Bool = false
+    let userObject = PFObject(className: "User")
+
+    init() {
+        // Initialize
+    }
+
+    
+
+}
