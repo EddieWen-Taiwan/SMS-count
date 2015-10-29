860088a258db8831109779e14c01ac97a7e1a3d4
EddieWen-Taiwan <m20021991y@gmail.com>
2015-10-29 16:41:00 +0800

    Keep testExample empty

--- SMSCountTests/SMSCountTests.swift
+++ SMSCountTests/SMSCountTests.swift
@@ -24,6 +24,9 @@ class SMSCountTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testSplit2Int() {
 
         let userinfo = UserInfo()
         var dateString = "2014 / 01 / 02"
