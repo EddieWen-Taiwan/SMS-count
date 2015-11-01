2ad36eaf43c51baa4dfe359acdb9bc3ecfcd9df0
EddieWen-Taiwan <m20021991y@gmail.com>
2015-11-01 20:03:16 +0800

    Let's test chackDaysAnimation()

--- SMSCountTests/ViewControllerTests.swift
+++ SMSCountTests/ViewControllerTests.swift
@@ -63,6 +63,10 @@ class ViewControllerTests: XCTestCase {
 
     }
 
+    func testCheckDaysAnimation() {
+        
+    }
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
