786a9f04bcbe9dd019789770f03e114afcf4c746
EddieWen-Taiwan <m20021991y@gmail.com>
2015-11-01 14:52:25 +0800

    Remove space line

--- SMSCountTests/ViewControllerTests.swift
+++ SMSCountTests/ViewControllerTests.swift
@@ -23,7 +23,7 @@ class ViewControllerTests: XCTestCase {
         self.targetViewController.loadView()
         self.targetViewController.viewDidLoad()
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
