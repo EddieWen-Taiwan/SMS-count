929a829f71d673324b05f5cc4a3e2f6809f7c3f7
EddieWen-Taiwan <m20021991y@gmail.com>
2015-11-04 16:41:44 +0800

    Add StatusViewController.swift

---
+++ SMSCount/StatusViewController.swift
@@ -0,0 +1,25 @@
+//
+//  StatusViewController.swift
+//  SMSCount
+//
+//  Created by Eddie on 11/4/15.
+//  Copyright © 2015 Wen. All rights reserved.
+//
+
+import UIKit
+
+class StatusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+
+    }
+
+}
