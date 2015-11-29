ea12e698c9c38191ea6634a0e51408e78d3b503c
EddieWen-Taiwan <m20021991y@gmail.com>
2015-11-29 15:48:17 +0800

    Switch two rootViewController( TabBarController & FriendListViewController ) successfully

--- SMSCount/LeftDrawerViewController.swift
+++ SMSCount/LeftDrawerViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import DrawerController
 
 class LeftDrawerViewController: UIViewController {
 
+    var storyborad: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+    var appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -22,9 +25,25 @@ class LeftDrawerViewController: UIViewController {
     }
     
     @IBAction func goMainTabBarController(sender: AnyObject) {
+        let rootViewController = self.storyboard?.instantiateViewControllerWithIdentifier("TabBarController")
+
+        self.switchRootViewController(rootViewController!)
     }
 
     @IBAction func goFriendListController(sender: AnyObject) {
+        let rootViewController = self.storyboard?.instantiateViewControllerWithIdentifier("FriendListController")
+
+        self.switchRootViewController(rootViewController!)
+    }
+
+    func switchRootViewController( rootViewController: UIViewController ) {
+
+        self.appDelegate.drawerController.centerViewController = rootViewController
+        self.appDelegate.drawerController.toggleDrawerSide( DrawerSide.Left, animated: true, completion: { (success: Bool) -> Void in
+            print(success)
+            print("Done")
+        })
+
     }
 
     /*
