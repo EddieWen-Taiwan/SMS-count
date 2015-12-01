fdd634b8dad716da790dcfe0539e037e4768b647
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-01 09:57:37 +0800

    TypeSetting

--- SMSCount/SettingViewController.swift
+++ SMSCount/SettingViewController.swift
@@ -353,6 +353,7 @@ class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerV
                                         let noAction = UIAlertAction(title: "否", style: .Cancel, handler: nil)
                                         syncAlertController.addAction(yesAction)
                                         syncAlertController.addAction(noAction)
+
                                         self.presentViewController( syncAlertController, animated: true, completion: nil)
 
                                     }
