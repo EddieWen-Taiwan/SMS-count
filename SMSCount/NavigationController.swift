13798c28d3bbf324f4b677ac8a829db2deabfe52
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-04 09:47:37 +0800

    Successfully add UIBarButtonItem on navigation bar Use UInavigationController.childViewController

--- SMSCount/NavigationController.swift
+++ SMSCount/NavigationController.swift
@@ -13,11 +13,20 @@ class NavigationController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        self.navigationBar.barTintColor = UIColor.redColor()
+
+        let drawerItem = UIBarButtonItem(barButtonSystemItem: .Camera, target: self, action: "test")
+        self.childViewControllers.first?.navigationItem.setLeftBarButtonItem( drawerItem, animated: true )
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func test() {
+        print("OOOO")
     }
 
     /*
