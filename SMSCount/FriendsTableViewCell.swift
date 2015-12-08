e03d6fb46e8deff6902d4f3a130282b7d284bf78
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-08 21:55:47 +0800

    Add FriendsTableViewCell.swift

---
+++ SMSCount/FriendsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendsTableViewCell.swift
+//  SMSCount
+//
+//  Created by Eddie on 12/8/15.
+//  Copyright © 2015 Wen. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
