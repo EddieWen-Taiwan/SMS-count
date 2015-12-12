fd89d16ad54347f7b804ac6c3d32cfdc78f108fc
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-12 14:42:16 +0800

    Function return Booleab fron super.isSettingAllDone

--- SMSCount/FriendsCalculate.swift
+++ SMSCount/FriendsCalculate.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class FriendsCalculate: CalculateHelper {
 
-    func inputFriendData( enterDate: String?, serviceDays: Int?, discountDays: Int = 0, autoFixed: Bool = false ) {
+    func inputFriendData( enterDate: String?, serviceDays: Int?, discountDays: Int = 0, autoFixed: Bool = false ) -> Bool {
 
-        
+        self.valueEnterDate = enterDate != nil ? enterDate! : ""
+        self.valueServiceDays = serviceDays != nil ? serviceDays! : -1
+        self.valueDiscountDays = discountDays
+        self.valueAutoFixed = autoFixed
+
+        if super.isSettingAllDone() {
+            super.updateDate()
+            return true
+        } else {
+            return false
+        }
 
     }
 
