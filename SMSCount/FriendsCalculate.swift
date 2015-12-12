971da2d9947da2ebc6fede528cb7fe2e3f18d8ba
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-12 14:13:11 +0800

    Add FriendsCalculate.swift

---
+++ SMSCount/FriendsCalculate.swift
@@ -0,0 +1,15 @@
+//
+//  FriendsCalculate.swift
+//  SMSCount
+//
+//  Created by Eddie on 12/12/15.
+//  Copyright © 2015 Wen. All rights reserved.
+//
+
+import UIKit
+
+class FriendsCalculate: CalculateHelper {
+
+    
+
+}
