c28090f0cf47b857b1e8066ca26f1ce1eebec5f2
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-20 20:47:29 +0800

    Add a new TableViewController

---
+++ SMSCount/SettingTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingTableViewController.swift
+//  SMSCount
+//
+//  Created by Eddie on 12/20/15.
+//  Copyright © 2015 Wen. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
