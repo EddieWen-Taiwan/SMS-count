4b5055527c3d457b07bf0b1dd9234ca1d165d17b
EddieWen-Taiwan <m20021991y@gmail.com>
2016-01-07 15:32:32 +0800

    Add FacebookLogin class

---
+++ SMSCount/FacebookLogin.swift
@@ -0,0 +1,13 @@
+//
+//  FacebookLogin.swift
+//  SMSCount
+//
+//  Created by Eddie on 1/7/16.
+//  Copyright © 2016 Wen. All rights reserved.
+//
+
+import FBSDKLoginKit
+
+class FacebookLogin {
+
+}
