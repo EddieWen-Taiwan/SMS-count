dd02ae77735b5323d9aad32484e9e2e850a4b19c
EddieWen-Taiwan <m20021991y@gmail.com>
2016-01-22 20:35:04 +0800

    Initialize CoverView

--- SMSCount/CoverView.swift
+++ SMSCount/CoverView.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class CoverView: UIView {
 
-    
+    var viewWidth: CGFloat = 0.0
+    var viewHeight: CGFloat = 0.0
+
+    convenience init() {
+        self.init(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.width-44-49))
+
+        viewWidth = UIScreen.mainScreen().bounds.width
+        viewHeight = UIScreen.mainScreen().bounds.height-44-49
+
+    }
 
 }
