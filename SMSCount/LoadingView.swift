646816dbe01ca4e3ff133269c9af5ebf285b3a91
EddieWen-Taiwan <m20021991y@gmail.com>
2016-01-22 09:56:19 +0800

    Add LoadingView class for UIObject

---
+++ SMSCount/LoadingView.swift
@@ -0,0 +1,15 @@
+//
+//  LoadingView.swift
+//  SMSCount
+//
+//  Created by Eddie on 1/22/16.
+//  Copyright © 2016 Wen. All rights reserved.
+//
+
+import UIKit
+
+class LoadingView: UIView {
+
+    
+
+}
