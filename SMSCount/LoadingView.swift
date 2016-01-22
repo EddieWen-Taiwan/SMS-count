69798ffc431828bbf6d8480c699a48683d1fcd8e
EddieWen-Taiwan <m20021991y@gmail.com>
2016-01-22 10:06:13 +0800

    LoadingView is completed

--- SMSCount/LoadingView.swift
+++ SMSCount/LoadingView.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class LoadingView: UIView {
 
-    
+    convenience init(center: CGPoint) {
+        self.init(frame: CGRectMake(0,0,80,80))
+
+        self.center = center
+        self.layer.cornerRadius = 20
+        self.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7)
+
+        let indicator = self.addActivityIndicator()
+        self.addSubview( indicator )
+    }
+
+    // Add UIActivityIndicator in UIView center
+    func addActivityIndicator() -> UIActivityIndicatorView {
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .WhiteLarge)
+            activityIndicator.center = CGPointMake(40, 40)
+
+        return activityIndicator
+    }
 
 }
