629ca80013085dd620ea39db034ddff68f8d5ae4
EddieWen-Taiwan <m20021991y@gmail.com>
2016-01-22 20:24:44 +0800

    New class - CoverView

---
+++ SMSCount/CoverView.swift
@@ -0,0 +1,15 @@
+//
+//  CoverView.swift
+//  SMSCount
+//
+//  Created by Eddie on 1/22/16.
+//  Copyright © 2016 Wen. All rights reserved.
+//
+
+import UIKit
+
+class CoverView: UIView {
+
+    
+
+}
