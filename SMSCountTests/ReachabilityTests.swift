f13dd12193f81a9555d4c7ffb0d3dcf1f9f83542
EddieWen-Taiwan <m20021991y@gmail.com>
2016-02-04 22:01:26 +0800

    Init Reachability

--- SMSCountTests/ReachabilityTests.swift
+++ SMSCountTests/ReachabilityTests.swift
@@ -10,22 +10,26 @@ import XCTest
 @testable import SMSCount
 
 class ReachabilityTests: XCTestCase {
-    
+
+    var reachability: Reachability?
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        self.reachability = Reachability()
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
