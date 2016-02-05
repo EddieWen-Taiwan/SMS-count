392ec003e911c6b35b98158977d49ba1ff4f8b70
EddieWen-Taiwan <m20021991y@gmail.com>
2016-02-05 20:29:55 +0800

    Add class variable

--- SMSCountTests/UserInfoTests.swift
+++ SMSCountTests/UserInfoTests.swift
@@ -10,27 +10,31 @@ import XCTest
 @testable import SMSCount
 
 class UserInfoTests: XCTestCase {
+
+    let userPreference = NSUserDefaults( suiteName: "group.EddieWen.SMSCount" )!
+    var info: UserInfo!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.info = UserInfo()
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
