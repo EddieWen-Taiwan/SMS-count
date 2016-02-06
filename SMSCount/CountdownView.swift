ab51d85b607ff247dcd64469cedef0fd7a84a90e
EddieWen-Taiwan <m20021991y@gmail.com>
2016-02-07 01:23:15 +0800

    Add function to add day label

--- SMSCount/CountdownView.swift
+++ SMSCount/CountdownView.swift
@@ -10,8 +10,24 @@ import UIKit
 
 class CountdownView: UIView {
 
+    var dayLabel = UILabel()
+
     convenience init(view: UIView) {
-        self.init(frame: CGRectMake(0, 0, view.frame.width, view.frame.height-44-49))
+        self.init(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height-44-49))
+
+        self.addDaysLabel()
     }
 
+    func addDaysLabel() {
+
+        self.dayLabel = UILabel(frame: CGRectMake(0, 0, self.frame.width, 60))
+        self.dayLabel.center = self.center
+        self.dayLabel.text = "0"
+        self.dayLabel.font = UIFont(name: "Verdana", size: 74)
+        self.dayLabel.textColor = UIColor.whiteColor()
+        self.dayLabel.textAlignment = .Center
+
+        self.addSubview( self.dayLabel )
+
+    }
 }
