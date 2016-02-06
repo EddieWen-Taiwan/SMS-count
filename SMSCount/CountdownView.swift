850d805518422db0e35dfdc884a5b355a85fe3b6
EddieWen-Taiwan <m20021991y@gmail.com>
2016-02-06 23:42:21 +0800

    Add CountdownView.swift

---
+++ SMSCount/CountdownView.swift
@@ -0,0 +1,13 @@
+//
+//  CountdownView.swift
+//  SMSCount
+//
+//  Created by Eddie on 2/6/16.
+//  Copyright © 2016 Wen. All rights reserved.
+//
+
+import UIKit
+
+class CountdownView: UIView {
+
+}
