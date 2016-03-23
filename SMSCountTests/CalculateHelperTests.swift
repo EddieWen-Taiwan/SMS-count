55596a55740c0650a12a66a3ec1b940cc8b1cba4
EddieWen-Taiwan <m20021991y@gmail.com>
2016-03-23 09:32:43 +0800

    '++' is deprecated: it will be removed in Swift 3

--- SMSCountTests/CalculateHelperTests.swift
+++ SMSCountTests/CalculateHelperTests.swift
@@ -49,7 +49,7 @@ class CalculateHelperTests: XCTestCase {
 
     func testIsRetireDateFixed() {
 
-        for var i = 0; i < self.dataSample.count; i++ {
+        for var i = 0; i < self.dataSample.count; i+=1 {
             self.setDataSample( self.dataSample[i] )
             let helper = CalculateHelper()
 
@@ -60,7 +60,7 @@ class CalculateHelperTests: XCTestCase {
 
     func testGetRetireDate() {
 
-        for var i = 0; i < self.dataSample.count; i++ {
+        for var i = 0; i < self.dataSample.count; i+=1 {
             self.setDataSample( self.dataSample[i] )
             let helper = CalculateHelper()
 
